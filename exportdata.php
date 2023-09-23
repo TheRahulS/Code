29be6b2c351ae19aa9af609932bd85e4641b7934
TheRahulS <rahulsoni7982@gmail.com>
2023-09-23 08:32:40 +0530

    two file are added

--- exportdata.php
+++ exportdata.php
@@ -4,7 +4,7 @@ $conn = mysqli_connect('localhost:3306', 'root', '', 'pdfdata');
 $sql = "SELECT * FROM csv_storage";
 $result = mysqli_query($conn, $sql);
 $csvFileName = "exported_data.csv";
-$csvFile = fopen($csvFileName, 'w');
+$csvFile = fopen($csvFileName, 'w');  
 fputcsv($csvFile, ["name", "nationality", "city","latitude","longitude","gender"]);
 while ($row = mysqli_fetch_assoc($result)) {
     // fputcsv($csvFile, [$row['name'], $row['nationality'], $row['city'],$row['latitude'],$row['longitude'],$row['gender']]);
@@ -17,4 +17,6 @@ readfile($csvFileName);
 unlink($csvFileName);
 mysqli_close($conn);
 exit();
+
+// njhjk
 ?>
